db8ba81d537935a89e948a24da948541c00127f9
Greg Ziegan <grz5@case.edu>
2014-03-30 20:46:29 -0400

    bitmap converter puts all bmp images into .data list

---
+++ battleforts.py
@@ -0,0 +1,38 @@
+from src.bitmap_converter import convert_bitmap
+from glob import glob
+import os
+import subprocess
+
+def prepare_battleforts():
+    print "Preparing battleforts..."
+
+    pixels = []
+    bitmap_files = glob("img/bitmap/*.bmp")
+    bitmap_files = [ os.path.abspath(rel_path) for rel_path in bitmap_files ]
+    print "Converting bitmaps to pixel array..."
+    num_bitmap_files = len(bitmap_files)
+    for i in range(num_bitmap_files):
+        print "Converting bmp {}/{}".format(i+1, num_bitmap_files)
+        pixels.extend(convert_bitmap(bitmap_files[i]))
+
+    main_partial_lines = []
+    with open("src/main.asm") as f:
+        for line in f.readlines():
+            if '.data' in line:
+                continue
+            main_partial_lines.append(line)
+
+    main_partial_source = "".join(main_partial_lines)
+
+    print "Injecting bitmap data into battleforts assembly..."
+    with open("src/main_complete.asm", 'w') as f:
+        f.write(".data\n")
+        f.write("list: .word {}\n".format(",".join(pixels)))
+        f.write(main_partial_source)
+
+    print "All ready, launching MARS simulator!"
+    subprocess.Popen(["java", "-jar", "Mars4_4.jar"])
+
+
+if __name__ == '__main__':
+    prepare_battleforts()
---
+++ src/bitmap_converter.py
@@ -0,0 +1,19 @@
+from PIL import Image
+
+def convert_bitmap(filepath):
+    im = Image.open(filepath)
+
+    width = im.size[0]
+    height = im.size[1]
+
+    pixels = []
+
+    # currently reducing to 1/10 number of original pixels
+    for x in xrange(0, width, 10):
+        for y in xrange(0, height, 10):
+            pixels.append(rgb_to_hex(im.getpixel((x,y))))
+
+    return pixels
+
+def rgb_to_hex(rgb):
+    return '%02x%02x%02x' % rgb
