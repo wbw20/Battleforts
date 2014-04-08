10c9ea36d3f7851009b060e612e5bb87adf1aa14
Greg Ziegan <grz5@case.edu>
2014-04-08 19:33:51 -0400

    work in progress

--- src/main.asm
+++ src/main.asm
@@ -1,5 +1,5 @@
 .data
-
+list:           .word 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00, 00ff00
 # colors
 red:            .word 0xff0000
 green:          .word 0x00ff00
