292c471c257352a848457f9857770ad6b480844e
Greg Ziegan <grz5@case.edu>
2014-04-15 21:06:36 -0400

    loading data on to the heap

--- src/main.asm
+++ src/main.asm
@@ -1,5 +1,7 @@
 .data
 
+fin: .asciiz "bmp_colors.txt"      # filename for input
+
 # colors
 red:            .word 0xff0000
 green:          .word 0x00ff00
@@ -14,11 +16,13 @@ display:        .space 0x4000
 
 .text
 
+jal loadImageData
+
 li $t0, 0
 li $t1, 0
 li $t2, 42
 li $t3, 63
-la $t4, MongolWalk1
+la $t4, 0($v0)
 addi $sp, $sp, -20
 sw $t0, 0($sp)
 sw $t1, 4($sp)
@@ -29,6 +33,38 @@ sw $t4, 16($sp)
 jal drawBitmap
 
 j exit
+
+
+loadImageData:
+
+  li   $v0, 13       # system call for open file
+  la   $a0, fin      # board file name
+  li   $a1, 0        # Open for reading
+  li   $a2, 0
+  syscall            # open a file (file descriptor returned in $v0)
+  move $s6, $v0      # save the file descriptor 
+
+  li $a0, 1024
+  li $v0 9 # syscall 9 (sbrk)
+  syscall
+
+  la $t0, 0($v0)  # address for start of heap memory
+
+  #read from file
+  li   $v0, 14       # system call for read from file
+  move $a0, $s6      # file descriptor 
+  la   $a1, 0($t0)   # address of buffer to which to read
+  li   $a2, 10000     # hardcoded buffer length
+  syscall            # read from file
+
+  # Close the file 
+  li   $v0, 16       # system call for close file
+  move $a0, $s6      # file descriptor to close
+  syscall            # close file
+
+  la $v0, 0($t0)
+  jr $ra
+
 
 #
 #  Parameters:
@@ -65,79 +101,39 @@ drawPixel:
 #    0($sp) --> x coordinate
 #    4($sp) --> y coordinate
 #    8($sp) --> width
-#    12($sp) --> height
-#    16($sp) --> color hex code
-#
-drawFilledRectangle:
-
-  lw $s0, 0($sp)
-  lw $s1, 4($sp)
-  lw $s2, 8($sp)
-
-  lw $a2, 16($sp)
-
-  dfr_row:
-    # at the end of this row?
-    bltz $s2, dfr_return
-    lw $s3, 12($sp)
-    add $a0, $s0, $s2 # x coordinate
-
-    dfr_column:
-      # at the end of this column?
-      bltz $s3, dfr_end_row
-      add $a1, $s1, $s3 # y coordinate
-
-      jal drawPixel
-
-      addi $s3, $s3, -1
-      j dfr_column
-
-  dfr_end_row:
-    addi $s2, $s2, -1
-    j dfr_row
-
-  dfr_return:
-    jr $ra # return
-
-
-#
-#  Parameters:
-#    0($sp) --> x coordinate
-#    4($sp) --> y coordinate
-#    8($sp) --> width
 #   12($sp) --> height
 #   16($sp) --> address of bitmap pixel colors
 #
 drawBitmap:
 
-  lw $s0, 0($sp)
-  lw $s1, 4($sp)
-  lw $s2, 8($sp)
-  lw $s3, 12($sp)
-  lw $s4, 16($sp)
+  lw $t0, 0($sp)
+  lw $t1, 4($sp)
+  lw $t2, 8($sp)
+  lw $t3, 12($sp)
+  lw $t4, 16($sp)
 
   db_row:
     # at the end of this row?
-    bltz $s2, dfr_return
-    lw $s3, 12($sp)
-    add $a0, $s0, $s2 # x coordinate
+    bltz $t2, db_return
+    lw $t3, 12($sp)
+    add $a0, $t0, $t2 # x coordinate
 
     db_column:
       # at the end of this column?
-      bltz $s3, db_end_row
-      add $a1, $s1, $s3 # y coordinate
+      bltz $t3, db_end_row
+      add $a1, $t1, $t3 # y coordinate
 
-      lw $a2, 0($s4) # load color
+      lw $a2, 0($t4) # load color
 
       jal drawPixel
 
-      addi $s4, $s4, 4 # increment color address
+      addi $t4, $t4, 4 # increment color address
 
-      addi $s3, $s3, -1
+      addi $t3, $t3, -1
       j db_column
 
   db_end_row:
-    addi $s2, $s2, -1
+    addi $t2, $t2, -1
     j db_row
 
   db_return:
