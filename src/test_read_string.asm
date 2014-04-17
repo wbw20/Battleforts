c9ab503882c2b76b2d9f07db7c834b13b67806cb
Greg Ziegan <grz5@case.edu>
2014-04-17 17:06:08 -0400

    using the stack for non-leaf procedure

--- src/test_read_string.asm
+++ src/test_read_string.asm
@@ -12,43 +12,53 @@ j exit
 
 # $a0 = address of string
 getColorValue:
-  la $s6, ($ra)   # return to main
-  move $s4, $a0
-  li $s7, 0       # return value
-  move $a0, $s4
+  li $v0, 0           # return value
+  subu $sp, $sp, 12
+  sw $v0, ($sp)       # store return value
+  sw $a0, 4($sp)      # store address of string
+  sw $ra, 8($sp)      # store return address
   jal getColorComponentValue
   move $t0, $v0
+  sll $t0, $t0, 8       # multiply first position by 256 * 256
   sll $t0, $t0, 8
-  sll $t0, $t0, 8
-  add $s7, $s7, $t0
-  la $a0, 2($s4)
+  lw $v0, ($sp)
+  add $v0, $v0, $t0
+  sw $v0, ($sp)         # store new return value
+  lw $t1, 4($sp)        # load string address
+  la $a0, 2($t1)        # offset address for next two characters
   jal getColorComponentValue
   move $t0, $v0
-  sll $t0, $t0, 8
-  add $s7, $s7, $t0
-  la $a0, 4($s4)
+  sll $t0, $t0, 8       # multiply second position by 256
+  lw $v0, ($sp)
+  add $v0, $v0, $t0
+  sw $v0, ($sp)
+  lw $t1, 4($sp)
+  la $a0, 4($t1)
   jal getColorComponentValue
-  add $s7, $s7, $v0
-  move $v0, $s7
-  jr $s6
+  move $t0, $v0
+  lw $v0, ($sp)
+  add $v0, $v0, $t0
+  lw $ra, 8($sp)
+  addu $sp, $sp, 12
+  jr $ra
 
 # $a0 = address of first charcter
 getColorComponentValue:
-  la $s0, ($ra)		# original return address
-  li $s1, 0       	# return value = 0
-  la $s2, ($a0)		# character address
-  lb $t3, 0($s2)	# character
+  la $s0, ($ra)		       # original return address
+  li $s1, 0       	     # return value = 0
+  la $s2, ($a0)		       # character address
+  lb $t3, 0($s2)	       # character
   move $a0, $t3
   jal charToDecValue
   move $t4, $v0
   sll $t4, $t4, 4
-  add $s1, $s1, $t4	# return value += val(str[0]) * 16 
+  add $s1, $s1, $t4	     # return value += val(str[0]) * 16 
   
   lb $t3, 1($s2)
   move $a0, $t3
   jal charToDecValue
   move $t4, $v0
-  add $s1, $s1, $t4	# return value += val(str[1])
+  add $s1, $s1, $t4	     # return value += val(str[1])
   move $v0, $s1
   jr $s0
 
