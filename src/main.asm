fbd55140e8dbf64d6003e312279259caad66d65c
nbudge <nab75@case.edu>
2014-04-18 15:29:52 -0400

    Added menu code

--- src/main.asm
+++ src/main.asm
@@ -1,10 +1,5 @@
 .data
 
-# colors
-red:            .word 0xff0000
-green:          .word 0x00ff00
-blue:           .word 0x0000ff
-
 # constants
 height:         .word 512
 width:          .word 512
@@ -14,19 +9,22 @@ display:        .space 0x4000
 
 .text
 
-main_init:
+main_menu:
 	
-	#li $t0, 0 #0
-	#li $t1, 0 #0
-	#li $t2, 42 
-	#li $t3, 64 
-	#la $t4, MongolWalk1
+	li $t0, 80 
+	li $t1, 0
+	li $t2, 327 
+	li $t3, 95 
+	la $t4, Battleforts #Battleforts
 	addi $sp, $sp, -20
-	#sw $t0, 0($sp)
-	#sw $t1, 4($sp)
-	#sw $t2, 8($sp)
-	#sw $t3, 12($sp)
-	#sw $t4, 16($sp)
+	sw $t0, 0($sp)
+	sw $t1, 4($sp)
+	sw $t2, 8($sp)
+	sw $t3, 12($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
+	nop
+
 
 main_waitLoop:
 	
@@ -38,36 +36,72 @@ main_waitLoop:
 	nop
 
 
-main_draw:
-	
-	jal getPlayer
+main_play:
+	jal getInput
 	nop
 	or $t6, $zero, $v0	# Backup direction from keyboard
+	
+	beq $t6, 0x04000000, main_exit
+	nop
+	
+	bne $t6, 0x01000000, main_play
+	nop
+	
+	
+main_instructions:
+	li $t0, 50
+	li $t1, 0
+	li $t2, 393 
+	li $t3, 104 
+	la $t4, Instructions #Battleforts
+	addi $sp, $sp, -20
+	sw $t0, 0($sp)
+	sw $t1, 4($sp)
+	sw $t2, 8($sp)
+	sw $t3, 12($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
+	nop
+	
 
 
-main_drawLeft:
-	bne $t6, 0x01000000, main_drawRight
+main_generateUnits:
+	
+	jal getInput
+	nop
+	or $t6, $zero, $v0	# Backup direction from keyboard
+	
+	beq $t6, 0x04000000, main_exit
+	nop
+
+
+main_generateUnitsLeft:
+	bne $t6, 0x02000000, main_generateUnitsRight
 	nop
 	b paladinWalk
 	nop
 	
 	
-main_drawRight:
-	bne $t6, 0x02000000, main_exit
+main_generateUnitsRight:
+	bne $t6, 0x03000000, main_done
 	nop
 	b mongolWalk
 	nop
+
+	
+main_done:
+	b main_generateUnits
 	
 
 main_exit:
-	bne $t6, 0x03000000, main_draw
-	nop
     li $v0, 10
     syscall
 
+
+
 mongolWalk:
-	li $t0, 700 
-	li $t1, 310 
+	li $t0, 450 
+	li $t1, 175 
 	sw $t0, 0($sp)
 	sw $t1, 4($sp)
 	
@@ -108,12 +142,12 @@ mongolWalk:
 	jal drawBitmap
 	nop
 	
-	j main_draw
+	j main_generateUnits
 	nop
 
 paladinWalk:
-	li $t0, -250 
-	li $t1, 285 
+	li $t0, 0 
+	li $t1, 150 
 	sw $t0, 0($sp)
 	sw $t1, 4($sp)
 	
@@ -153,7 +187,7 @@ paladinWalk:
 	jal drawBitmap
 	nop
 	
-	j main_draw
+	j main_generateUnits
 	nop
 
 
@@ -186,45 +220,6 @@ drawPixel:
   sw $a2, display($t2)
 
   jr $ra # return
-
-#
-#  Parameters:
-#    0($sp) --> x coordinate
-#    4($sp) --> y coordinate
-#    8($sp) --> width
-#    12($sp) --> height
-#    16($sp) --> color hex code
-#
-drawFilledRectangle:
-
-  lw $s0, 0($sp)
-  lw $s1, 4($sp)
-  lw $s2, 8($sp)
-
-  lw $a2, 16($sp)
-
-  dfr_row:
-    # at the end of this row?
-    bltz $s2, dfr_return
-    lw $s3, 12($sp)
-    add $a0, $s0, $s2 # x coordinate
-
-    dfr_column:
-      # at the end of this column?
-      bltz $s3, dfr_end_row
-      add $a1, $s1, $s3 # y coordinate
-
-      jal drawPixel
-
-      addi $s3, $s3, -1
-      j dfr_column
-
-  dfr_end_row:
-    addi $s2, $s2, -1
-    j dfr_row
-
-  dfr_return:
-    jr $ra # return
 
 
 #
@@ -298,29 +293,34 @@ sleep:
 #  Returns:
 #	 v0 = player(side) OR exit program
 #
-getPlayer:
+getInput:
 	lw $t0, 0xFFFF0004		# Load input value
-
-getPlayer_left:
-	bne $t0, 102, getPlayer_right
+getInput_play:
+	bne $t0, 112, getInput_left
 	nop
-	ori $v0, $zero, 0x01000000	# left
-	j getPlayer_done
+	ori $v0, $zero, 0x01000000	# play
+	j getInput_done
 	nop
-getPlayer_right:
-	bne $t0, 106, getPlayer_exit
+getInput_left:
+	bne $t0, 102, getInput_right
 	nop
-	ori $v0, $zero, 0x02000000	# right
-	j getPlayer_done
+	ori $v0, $zero, 0x02000000	# left
+	j getInput_done
 	nop
-getPlayer_exit:
-	bne $t0, 120, getPlayer_none
+getInput_right:
+	bne $t0, 106, getInput_exit
 	nop
-	ori $v0, $zero, 0x03000000	# exit
-	j getPlayer_done
+	ori $v0, $zero, 0x03000000	# right
+	j getInput_done
 	nop
-getPlayer_none:
+getInput_exit:
+	bne $t0, 120, getInput_none
+	nop
+	ori $v0, $zero, 0x04000000	# exit
+	j getInput_done
+	nop
+getInput_none:
 						# Do nothing
-getPlayer_done:
+getInput_done:
 	jr $ra				# Return
 	nop	
