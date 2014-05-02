1ad8d4b20ef4a19f3b02c2d3d526ec3e40f4d388
nbudge <nab75@case.edu>
2014-05-01 21:31:25 -0400

    Movement using paladinWalk and mongolWalk

--- src/main.asm
+++ src/main.asm
@@ -5,7 +5,6 @@ height:         .word 512
 width:          .word 512
 word_size:      .word 4
 hot_pink:       .word 0xff0080
-clear:					.word 0X000000
 
 display:        .space 0x4000
 data_pointer:   .word 0x103df050
@@ -74,13 +73,11 @@ main_clearMenu:
 	li $t1, -175																		# y position
 	li $t2, 327 																		# width
 	li $t3, 95 																			# height
-	la $t4, clear																		# bitmap label
 	sw $t0, 0($sp)
 	sw $t1, 4($sp)
 	sw $t2, 8($sp)
 	sw $t3, 12($sp)
-	sw $t4, 16($sp)
-	jal drawBitmap	
+	jal clearBitmap
 
 
 #
@@ -128,7 +125,7 @@ main_instructions_userInput:
 
 	j main_instructions_userInput										# Loop until valid key is pressed 
 
- 
+
 #
 # Desc:
 #		Clear instruction menu
@@ -143,13 +140,11 @@ main_clearInstructions:
 	li $t1, -175																		# y position
 	li $t2, 393 																		# width
 	li $t3, 104 																		# height
-	la $t4, clear 																	# bitmap label
 	sw $t0, 0($sp)
 	sw $t1, 4($sp)
 	sw $t2, 8($sp)
 	sw $t3, 12($sp)
-	sw $t4, 16($sp)
-	jal drawBitmap
+	jal clearBitmap
 	
 	j main_generateUnits														# Jumping becuase user has already inputed a key
 
@@ -193,13 +188,7 @@ main_generateUnits:
 #
 main_generateUnitsLeft:
   bne $t6, 0x02000000, main_generateUnitsRight
-  nop
-
-  # store a paladin
-  la $a0, PaladinWalk1
-  li $a1, -50
-  jal store_unit
-
+	b paladinWalk
 
 #
 # Desc:
@@ -211,67 +200,9 @@ main_generateUnitsLeft:
 #
 main_generateUnitsRight:
   bne $t6, 0x03000000, main_done
-  nop
-
-  # store a mongol
-  la $a0, MongolWalk1
-  li $a1, 440
-  jal store_unit
-	
-
-main_movement:
-	jal render									# render generate units
-	
-	lw $t0, data          			# bottom of unit stack
-	lw $t1, ($t0)								# get unit bitmap
-	
-	la $t2, MongolWalk1					# get MongolWalk1 bitmap 
-	beq $t1, $t2, mongolWalk2		# compare
-	
-	la $t2, MongolWalk2					# etc..
-	beq $t1, $t2, mongolWalk3
-	
-	la $t2, MongolWalk3
-	beq $t1, $t2, mongolWalk4
-	
-	la $t2, MongolWalk4
-	beq $t1, $t2, mongolWalk1
-	
-	b main_done
-	
-	
-	
-	mongolWalk2:
-		
-		la $a0, MongolWalk2
-		li $a1, 400
-		jal store_unit
-		
-		
-	mongolWalk3:
-		
-			la $a0, MongolWalk3
-			li $a1, 360
-			jal store_unit
-			
-	mongolWalk4:
-		
-			la $a0, MongolWalk4
-			li $a1, 320
-			jal store_unit
-			
-			
-	mongolWalk1:
-
-			la $a0, MongolWalk1
-			li $a1, 290
-			jal store_unit
-			
-			
+	b mongolWalk
 
 	
-	
-
 #
 # Desc:
 # 	Render unit and return to wait for user input
@@ -281,7 +212,6 @@ main_movement:
 #		none
 #
 main_done:
-  jal render
   b main_generateUnits_userInput
 
 
@@ -299,91 +229,210 @@ main_exit:
 
 
 
+#
+# Desc:
+# 	Moves paladins across the display
+# Parameters:
+#		none
+# Returns:
+#		none
+#
+paladinWalk:
+	li $t0, -50			# x position
+	li $t1, 50			# y position
+	li $t2, 86 			# width
+	li $t3, 107 		# height
+	sw $t0, 0($sp)
+	sw $t1, 4($sp)
+	sw $t2, 8($sp)
+	sw $t3, 12($sp)
+
+	la $t4, PaladinWalk1
+	sw $t4, 16($sp)
+	jal drawBitmap
+
+	jal clearBitmap
+
+	li $t0, 0				# x position
+	la $t4, PaladinWalk2
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
+
+	jal clearBitmap
+
+	li $t0, 50				# x position
+	la $t4, PaladinWalk3
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
+
+	jal clearBitmap
+
+	li $t0, 100				# x position
+	la $t4, PaladinWalk4
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
+
+	jal clearBitmap
+	
+	li $t0, 150				# x position
+	la $t4, PaladinWalk1
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
+
+	jal clearBitmap
+	
+	li $t0, 200				# x position
+	la $t4, PaladinWalk2
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
+
+	jal clearBitmap
+	
+	li $t0, 250				# x position
+	la $t4, PaladinWalk3
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
+
+	jal clearBitmap
+
+	li $t0, 300				# x position
+	la $t4, PaladinWalk4
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
+
+	jal clearBitmap
+	
+	li $t0, 350				# x position
+	la $t4, PaladinWalk1
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
+
+	jal clearBitmap
+	
+	li $t0, 400				# x position
+	la $t4, PaladinWalk2
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
+
+	jal clearBitmap
+	
+	j main_generateUnits_userInput	
+
 
 #
-#  Parameters:
-#    none
-#  Returns:
-#   none
+# Desc:
+# 	Moves mongols across the display
+# Parameters:
+#		none
+# Returns:
+#		none
 #
-render:
-  lw $t0, data          # bottom of unit stack
-  lw $t1, data_pointer  # top of unit stack
+mongolWalk:
+	li $t0, 440			# x position
+	li $t1, 50			# y position  
+	li $t2, 86 			# width
+	li $t3, 107 		# height
+	sw $t0, 0($sp)
+	sw $t1, 4($sp)
+	sw $t2, 8($sp)
+	sw $t3, 12($sp)
 
-  addi $sp, $sp, -16
-  sw $ra, 0($sp)    # remember return address
-  sw $t0, 4($sp)    # remember bottom
-  sw $t1, 8($sp)    # remember top
-  sw $t0, 12($sp)    # remember preserved bottom
-  addi $sp, $sp, 16
+	la $t4, MongolWalk1
+	sw $t4, 16($sp)
+	jal drawBitmap
 
-  read_unit:
-    # retrive unit stack
-    addi $sp, $sp, -16
-    lw $t0, 4($sp)    # bottom
-    lw $t1, 8($sp)    # top
-    addi $sp, $sp, 16
+	jal clearBitmap
 
-    beq $t0, $t1, r_return # no more units
+	li $t0, 390			# x position
+	la $t4, MongolWalk2
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
 
-    lw $t2, ($t0) # get unit bitmap
-    lw $t3, 4($t0) # x position
-    lw $t4, 8($t0) # y position
-    lw $t5, 12($t0) # health
+	jal clearBitmap
 
-    li $t6, 86
-    li $t7, 107
+	li $t0, 340			# x position
+	la $t4, MongolWalk3
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
 
-    # do the drawing
-    sw $t3, ($sp)     # x position
-    sw $t4, 4($sp)    # y position
-    sw $t6, 8($sp)    # width
-    sw $t7, 12($sp)   # height
-    sw $t2, 16($sp)   # bitmap address
-    jal drawBitmap
+	jal clearBitmap
 
-    # update bottom pointer
-    addi $sp, $sp, -16
-    lw $t0, 4($sp)      # get bottom
-    addi $t0, $t0, 16   # increment bottom
-    sw $t0, 4($sp)      # store it back
-    addi $sp, $sp, 16
-    j read_unit
+	li $t0, 340			# x position
+	la $t4, MongolWalk4
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
 
-  r_return:
-    addi $sp, $sp, -16
-    lw $t0, 12($sp)
-    lw $t1, data
-    sw $t0, ($t1)  # replace the bottom of the unit stack
-    lw $ra, ($sp)
-    addi $sp, $sp, 16
-    jr $ra
+	jal clearBitmap
+	
+	li $t0, 290			# x position
+	la $t4, MongolWalk1
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
 
+	jal clearBitmap
 
-#
-#  Parameters:
-#    $a0 --> the bitmap address of the unit
-#    $a1 --> the starting x position
-#  Returns:
-#   none
-#
-store_unit:
-  lw $t0, data_pointer
+	li $t0, 240			# x position
+	la $t4, MongolWalk2
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
 
-  move $t1, $a0
-  sw $t1, ($t0)
-  addi $t0, $t0, 4
-  move $t1, $a1      # x position
-  sw $t1, ($t0)
-  addi $t0, $t0, 4
-  li $t1, 50         # y position
-  sw $t1, ($t0)
-  addi $t0, $t0, 4
-  li $t1, 100        # health
-  sw $t1, ($t0)
-  addi $t0, $t0, 4
-  sw $t0, data_pointer
-  jr $ra
+	jal clearBitmap
+
+	li $t0, 190			# x position
+	la $t4, MongolWalk3
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
+
+	jal clearBitmap
+
+	li $t0, 140			# x position
+	la $t4, MongolWalk4
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
+
+	jal clearBitmap
+	
+	li $t0, 90			# x position
+	la $t4, MongolWalk1
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
+
+	jal clearBitmap
+
+	li $t0, 40			# x position
+	la $t4, MongolWalk2
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
+
+	jal clearBitmap
+	
+	li $t0, -10			# x position
+	la $t4, MongolWalk3
+	sw $t0, 0($sp)
+	sw $t4, 16($sp)
+	jal drawBitmap
+
+	jal clearBitmap
+
+	j main_generateUnits_userInput	
 
 
 #
@@ -409,6 +458,8 @@ drawPixel:
 
   # total offset
   add $t2, $t2, $t3
+
+  lw $t5, display($t2)
 
   # draw
   sw $a2, display($t2)
@@ -469,6 +520,53 @@ drawBitmap:
     j db_row
 
   db_return:
+    jr $s7 # return
+
+#
+#  Parameters:
+#    0($sp) --> x coordinate
+#    4($sp) --> y coordinate
+#    8($sp) --> width
+#   12($sp) --> height
+#
+clearBitmap:
+  lw $s0, 0($sp)
+  lw $s1, 4($sp)
+  lw $s2, 8($sp)
+  lw $s3, 12($sp)
+
+  # x counter
+  li $s5, 0
+
+  # jal paints over $ra
+  move $s7, $ra
+
+  cb_row:
+    # at the end of this row?
+    beq $s2, $s5 cb_return
+
+    # y counter
+    li $s6, 0
+
+    lw $s3, 12($sp)
+    add $a0, $s0, $s5 # x coordinate
+
+    cb_column:
+      # at the end of this column?
+      beq $s3, $s6, cb_end_row
+      add $a1, $s1, $s6 # y coordinate
+
+      lw $a2, hot_pink # load color
+      jal drawPixel
+
+      addi $s6, $s6, 1
+      j cb_column
+
+  cb_end_row:
+    addi $s5, $s5, 1
+    j cb_row
+
+  cb_return:
     jr $s7 # return
 
 
